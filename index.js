254979c8e370d2e2ec1705bd7c529321740b275a
hadess-dotcom <igorschmitz51@gmail.com>
2026-05-24 19:23:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,45 @@ require('dotenv').config()
 const express = require('express')
 
 const {
-Client,
-GatewayIntentBits,
-ActivityType
+  Client,
+  GatewayIntentBits,
+  ActivityType
 } = require('discord.js')
 
 const app = express()
 
 const client = new Client({
-intents: [
-GatewayIntentBits.Guilds,
-GatewayIntentBits.GuildMessages,
-GatewayIntentBits.GuildMembers
-]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent
+  ]
 })
 
+app.use(express.json())
+
+// 🌐 Rota principal
 app.get('/', (req, res) => {
-res.send('🟣 CERBERUS STORE ONLINE')
+  res.send('🟣 CERBERUS STORE ONLINE')
 })
 
-client.once('ready', () => {
+// 🤖 Bot pronto
+client.once('clientReady', () => {
 
-console.log("🟣 ${client.user.tag} ONLINE")
+  console.log(`🟣 ${client.user.tag} ONLINE`)
 
-client.user.setActivity({
-name: 'CERBERUS STORE',
-type: ActivityType.Watching
+  client.user.setActivity({
+    name: 'CERBERUS STORE',
+    type: ActivityType.Watching
+  })
+
 })
-})
 
+// 🔑 Login bot
 client.login(process.env.DISCORD_BOT_TOKEN)
 
+// 🚀 API online
 app.listen(process.env.PORT || 3000, () => {
-console.log('🌐 API ONLINE')
+  console.log('🌐 API ONLINE')
 })
